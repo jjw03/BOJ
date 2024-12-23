07dc2a191494312f68e5ed436af514282ad75f2e
Programming-novice <59797757+Programming-novice@users.noreply.github.com>
2024-12-23 22:18:46 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <stack>
+#include <map>
+#include <functional>
+#include <cmath>
+#include <iomanip>
+#include <bitset>
+#include <numeric>
+#include <array>
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int tt;
+    cin >> tt;
+
+    vector<string> out;
+    for (int i = 0; i < tt; i++) {
+        string s = "";
+        for (int k = 0; k < i; k++) s.push_back(' ');
+        for (int k = 0; k < 2*(tt - i) - 1; k++) s.push_back('*');
+        out.push_back(s);
+    }
+    for (const auto& i : out) cout << i << '\n';
+    for (int i = tt - 2; i >= 0; i--) cout << out[i] << '\n';
+
+    return 0;
+}
