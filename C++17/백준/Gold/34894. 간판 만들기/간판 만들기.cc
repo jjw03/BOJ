2b01e4aa6a7c443d922052933d41a19cba0e2ab7
jjw03 <59797757+jjw03@users.noreply.github.com>
2025-12-07 21:33:47 +0900

    [Gold V] Title: 간판 만들기, Time: 40 ms, Memory: 6428 KB -BaekjoonHub

--- C++17/백준/Gold/34894. 간판 만들기/간판 만들기.cc
+++ C++17/백준/Gold/34894. 간판 만들기/간판 만들기.cc
@@ -6,38 +6,42 @@ typedef unsigned long long ull;
 using namespace std;
 
 /*
- * 각 문자들을 앞 문자들에 이어 붙여서 dp 구성
- * U -> UO -> UOS -> ... -> UOSPC
+ * 1차원 dp
+ * U -> UO -> ... -> UOSPC
  */
 
 int N;
 
 string str;
-ll cost[300'000];
-vector<vector<ll>> dp;
+int cost[300'000];
+vector<ll> dp;
 
 void init(){
     cin >> N;
-    dp.resize(5+1, vector<ll>(N+1, LLONG_MAX));
-    fill(dp[0].begin(), dp[0].end(), 0);
+    dp.resize(N + 1, LLONG_MAX);
 
     cin >> str;
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < N; i++){
         cin >> cost[i];
+        if(str[i] == 'U')    //dp 초기화
+            dp[i + 1] = 0;
+    }
 }
 
 void solve(){
     string ch = "UOSPC";
     for(int k = 1; k <= 5; k++){
         for(int i = 1; i <= N; i++){
-            if(str[i - 1] != ch[k - 1] || (k > 1 && dp[k - 1][i] == LLONG_MAX))
-                dp[k][i] = dp[k][i - 1];
+            //이어 붙일 수 없는 위치 or 이전 문자가 등장하지 않았음
+            if(str[i - 1] != ch[k - 1] || dp[i] == LLONG_MAX)
+                dp[i] = dp[i - 1];
+            //이어 붙일 수 있는 위치
             else
-                dp[k][i] = min(dp[k - 1][i] + cost[i - 1], dp[k][i - 1]);
+                dp[i] = min(dp[i] + cost[i - 1], dp[i - 1]);
         }
     }
 
-    cout << (dp[5][N] == LLONG_MAX ? -1 : dp[5][N]);
+    cout << (dp[N] == LLONG_MAX ? -1 : dp[N]);
 }
 
 int main(){
